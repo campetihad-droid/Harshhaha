f8072380c555fcdd6db25d9601097cc799beaf22
campetihad-droid <campetihad@gmail.com>
2026-07-07 21:47:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,12 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+from datetime import datetime, timedelta
+import random
+import asyncio
+import os
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHANNEL_ID = "@trackethiad"
+
+running = False
+used_users = []
